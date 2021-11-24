8041bc77186f6af7b4fdace8f0f00a9b11effb1a
patricia <patricia.poveda-boto@capgemini.com>
2021-11-24 13:50:30 +0100

    añadir proyecto

---
+++ src/main/java/com/formacionjava/springboot/apirest/TeamIiiApplication.java
@@ -0,0 +1,13 @@
+package com.formacionjava.springboot.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamIiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamIiiApplication.class, args);
+	}
+
+}
